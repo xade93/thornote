b6fe875238056bb79a4aa0d082c1bb535fc2a378
shi zhengyu <shizhengyu93@hotmail.com>
2026-06-27 01:45:06 +0800

    bump version no

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.thornotes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.2"
+        versionCode = 7
+        versionName = "0.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
